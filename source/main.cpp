afeda6b904204b5f61964e655ef89a706011cecc
Christopher Roy Bratusek <nano@tuxfamily.org>
2012-03-14 15:58:26 +0100

    also support NANDEmu-Boot apps-directory additionally to boot253

--- source/main.cpp
+++ source/main.cpp
@@ -250,10 +250,20 @@ main(int argc, char *argv[])
 			LoadHomebrew ("sd1:/apps/boot253/boot.dol");
 			BootHomebrew ();
 		}
+		else if (opendir(check_path("sd1:/apps/NANDEmu-Boot").c_str()) != NULL)
+		{
+			LoadHomebrew ("sd1:/apps/NANDEmu-Boot/boot.dol");
+			BootHomebrew ();
+		}
 		else if (opendir(check_path("usb1:/apps/boot253").c_str()) != NULL)
 		{
 			LoadHomebrew ("usb1:/apps/boot253/boot.dol");
 			BootHomebrew ();
+		}
+		else if (opendir(check_path("usb1:/apps/NANDEmu-Boot").c_str()) != NULL)
+		{
+			LoadHomebrew ("usb1:/apps/NANDEmu-Boot/boot.dol");
+			Boothomebrew ();
 		}
 	}
 
