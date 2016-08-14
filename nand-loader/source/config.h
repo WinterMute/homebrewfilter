da3d029cf687009b4de48f83292ed6c8e895ba6b
Christopher Roy Bratusek <nano@jpberlin.de>
2016-08-14 20:08:54 +0200

    revert new nand loader import

---
+++ nand-loader/source/config.h
@@ -0,0 +1,28 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* 'Config' structure */
+struct config {
+	/* Video modes */
+	bool forcePal50;
+	bool forcePal60;
+	bool forceNtsc;
+	bool forceHdtv;
+
+	/* Video mode patching */
+	bool patchVmode;
+
+	/* Splash screen */
+	bool showSplash;
+};
+
+/* NANDLoader configuration */
+extern struct config loaderCfg;
+
+/* Prototypes */
+s32  Config_Read(void);
+s32  Config_Write(void);
+void Config_Menu(void);
+
+#endif
+
