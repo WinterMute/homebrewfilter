3e4ba63812ddf017c2f3a184a7e3604eaf2b68b4
Christopher Roy Bratusek <nano@jpberlin.de>
2016-08-14 19:05:25 +0200

    replace nand loader mod 1.1 with fix94's tiny vwii nand loader

--- nand-loader/source/loader.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LOADER_H_
-#define _LOADER_H_
-
-/* Prototypes */
-s32 Loader_Execute(void);
-
-#endif
-
--- nand-loader/source/patches.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PATCHES_H_
-#define _PATCHES_H_
-
-/* Prototypes */
-void Patch_VideoMode(u8 *, u32);
-
-#endif
